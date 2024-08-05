48b640df0ab262743608a7257e6eaaf2ab4e68fa
Tarun Neeraj <tarunneeraj2000@gmail.com>
2024-08-06 00:46:05 +0530

    Starlette-admin added

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import database,models,JWTtoken,hashing
 from sqlmodel import Session,select
 from datetime import timedelta
 from routers import user_route, book_route, author_route, loan_route,notifaction_route
+from starlette_admin.contrib.sqla import Admin, ModelView
+import inspect
 
 app = FastAPI()
 
@@ -14,6 +16,28 @@ app.include_router(notifaction_route.router)
 app.include_router(loan_route.router)
 app.include_router(book_route.router)
 app.include_router(author_route.router)
+
+classlist = ["AuthorDetails", "LoanDetails", "UserDetails","BookCreate","AuthorCreate","AuthorUpdate","LoanApprovalRequest","LoanCancellationRequest","LoanReturnRequest","Login","Token","TokenData"]
+def create_admin_view(app):
+    # Create admin
+    admin = Admin(database.engine, title="Library Management System")
+
+    def get_classes_from_module():
+        # Dynamicimportlibrt the module
+        module =models
+        
+        # Filter and retrieve all classes defined in the module
+        # This checks if each class is defined in the module (compares the __module__ attribute)
+        classes = [member for name, member in inspect.getmembers(module, inspect.isclass)
+                if member.__module__ == "models"]
+        for cls in classes:
+            if not cls.__name__ in classlist:
+                admin.add_view(ModelView(cls))
+    get_classes_from_module()
+    # Mount admin to your app
+    admin.mount_to(app)
+
+create_admin_view(app)
 
 @app.get('/')
 def index():
