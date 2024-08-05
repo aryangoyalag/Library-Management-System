c8d5d3d3b9ab6215f33aad1eb27a8177f392b705
aryangoyalag <goyalaryan.ag@gmail.com>
2024-08-05 16:38:31 +0530

    databse update

--- database.py
+++ database.py
@@ -6,18 +6,15 @@ load_dotenv()
 
 # Get the database URL from the environment variable
 SQLALCHEMY_DB_URL = os.getenv("DATABASE_URL")
+engine = create_engine(SQLALCHEMY_DB_URL, connect_args={"check_same_thread": False})
 
-# Create the SQLModel engine
-engine = create_engine(SQLALCHEMY_DB_URL)
-
-# Initialize the database
+# Create the database tables
 def init_db():
     SQLModel.metadata.create_all(bind=engine)
 
-# Function to get a database session
 def get_db():
     db = Session(engine)
     try:
-        yield db
+        yield db  # Use a generator for context management
     finally:
         db.close()
